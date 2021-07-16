8d1138eeafe990052ae5f32ec278424f33deac67
ddoshi2021@gmail.com <ddoshi2021@gmail.com>
2021-07-16 07:39:31 -0700

    Add image

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -11,7 +11,7 @@ const Product = (props) => {
     const classes = useStyles();
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardMedia className={classes.media} image={product.image} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterButton>
@@ -20,7 +20,7 @@ const Product = (props) => {
                     <Typography variant="h5" gutterButton>
                         {product.price}
                     </Typography>
-                    <Typography variant="h2" color='textSecondary'>
+                    <Typography variant="body2" color='textSecondary'>
                         {product.description}
                     </Typography>
                 </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,8 +3,8 @@ import {Grid} from '@material-ui/core'
 import Product from './Product/Product'
 
 const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes', price: `$10`},
-    {id: 1, name: 'Sandals', description: 'Wedding Sandal', price: `$20`},
+    {id: 1, name: 'Shoes', description: 'Running shoes', price: `$10`, image: `https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`},
+    {id: 1, name: 'Sandals', description: 'Wedding Sandal', price: `$20`, image: `https://images.pexels.com/photos/273930/pexels-photo-273930.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`},
 ]
 const Products = () => {
     return (
