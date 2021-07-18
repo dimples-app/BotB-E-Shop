bf7b89005fcda4c0617c9a1416815a26df28fef5
ddoshi2021@gmail.com <ddoshi2021@gmail.com>
2021-07-17 22:17:22 -0700

    Add Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,29 @@
+import { AppBar, Toolbar, Typography, Badge, MenuItem, Menu, Typography, IconButton } from '@material-ui/core'
 import React from 'react'
+import {ShoppingCart} from '@material-ui/core'
+import logo from '../../Assets/e-shop.png'
+import { classes } from 'istanbul-lib-coverage'
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={} alt="Commerce.js" height="25px" className={classes.image} />
+                        Commerce.js
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button} >
+                        <IconButton aria-label="Show cart items" color="inherit" >
+                            <Badge badgeContent={2} color="secondary" >
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
     )
 }
 
