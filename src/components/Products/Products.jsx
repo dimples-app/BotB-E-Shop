bfe41904a1c824c4ba44ba4757a926bd14bc86b6
ddoshi2021@gmail.com <ddoshi2021@gmail.com>
2021-07-20 11:01:02 -0700

    Add products style

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Grid} from '@material-ui/core'
 import Product from './Product/Product'
+import useStyles from './styles';
 
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes', price: `$10`, image: `https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`},
     {id: 1, name: 'Sandals', description: 'Wedding Sandal', price: `$20`, image: `https://images.pexels.com/photos/273930/pexels-photo-273930.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`},
 ]
 const Products = () => {
+    const classes = useStyles();
     return (
-        <main>
+        <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4}>
             {
                 products.map((product) => (
