dfc3a3d54fd9f4569381faccab0ff6b75659dc9e
ddoshi2021@gmail.com <ddoshi2021@gmail.com>
2021-07-21 15:40:38 -0700

    Add hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Products, Navbar}  from "./components"
+import {commerce} from "./lib/commerce"
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  /**
+   * function to fetch product
+   */
+  const fetchProducts = async() => {
+    const response = await commerce.products.list();
+    const data = response.data;
+    setProducts(data)
+  }
+
+  /**
+   * UseEffect to call fetchcall without side effects
+   */
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
   return <>
   E - Shop
   <Navbar />
