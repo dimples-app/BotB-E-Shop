f606e3f7c1214f1e97bdf9510300a7e36bb1ac51
ddoshi2021 <ddoshi2021@gmail.com>
2021-09-06 12:39:25 -0700

    implement form hook

--- src/components/CheckoutForms/AddressForm.jsx
+++ src/components/CheckoutForms/AddressForm.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import FormInput from "./FormInput"
+
 
 function AddressForm() {
+    const method = useForm();
     return (
-        <div>
-            AddressForm
-        </div>
+        <>
+            <Typography variant="h6" gutterBottom>Shipping address</Typography>
+            <FormProvider {...method}>
+                <form onSubmit=''>
+                    <Grid container spacing={3}>
+                        <FormInput 
+                            required 
+                            name='firstName' 
+                            label='First Name' 
+                        />
+
+                        <FormInput 
+                            required 
+                            name='lastName' 
+                            label='Last Name' 
+                        />
+
+                        <FormInput 
+                            required 
+                            name='address' 
+                            label='Address' 
+                        />  
+
+                        <FormInput 
+                            required 
+                            name='email' 
+                            label='Email' 
+                        />
+
+                        <FormInput 
+                            required 
+                            name='city' 
+                            label='City' 
+                        />
+
+                        <FormInput 
+                            required 
+                            name='zip' 
+                            label='Zip/Poatal Code' 
+                        />
+
+                    </Grid>
+                    
+                </form>
+
+            </FormProvider>
+        </>
     )
 }
 
