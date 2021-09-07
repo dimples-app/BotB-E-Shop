fe25e3a9c98dc7a5bce199e5d64cdf8a0f3ad112
ddoshi2021 <ddoshi2021@gmail.com>
2021-09-07 08:17:19 -0700

    fetch more data

--- src/components/CheckoutForms/AddressForm.jsx
+++ src/components/CheckoutForms/AddressForm.jsx
@@ -13,20 +13,54 @@ function AddressForm({checkoutToken}) {
     const [shippingOptions, setShippingOptions] = useState([]);
     const [shippingOption, setShippingOption] = useState('');
 
+    //convert obj to arr and mapping over
     const countries =  Object.entries(shippingCountries).map(([code, name]) => (
         { id: code, label:name}
     ))
 
+    //convert obj to arr and mapping over
+    const subdivisions =  Object.entries(shippingSubdivisions).map(([code, name]) => (
+        { id: code, label:name}
+    ))
+
+    const options = shippingOptions.map((sO) => ({ id: sO.id, label: `${sO.description} - (${sO.price.formatted_with_symbol})` }))
+
+    // fetching countries from commerce
     const fetchShippingCountries = async (checkoutTokenId) => {
         const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
         setShippingCountries(countries);
         setShippingCountry(Object.keys(countries)[0]);
     };
 
+    // fetching subdivision from commerce
+    const fetSubDivisions = async(countrycode) => {
+        const { subdivisions } = await commerce.services.localeListSubdivisions(countrycode);
+        setShippingSubdivisions(subdivisions)
+        setShippingSubdivision(Object.keys(subdivisions)[0])
+    }
+
+    // fetching shipping options
+    const fetchShippingOptions = async (checkoutTokenId, country, stateProvince = null) => {
+        const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region: stateProvince });
+    
+        setShippingOptions(options);
+        setShippingOption(options[0].id);
+      };
+ 
+    // useeffect - fetching countries
     useEffect(() => {
         fetchShippingCountries(checkoutToken.id);
-      }, []);
+    }, []);
 
+    // useefect - fetching subdivision
+    useEffect(() => {
+        if(shippingCountry) fetSubDivisions(shippingCountry);
+    }, [shippingCountry]);
+
+    // useeffect - fetching options
+    useEffect(() => {
+        if(shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry,shippingSubdivision )
+    }, [shippingSubdivision])
     
     return (
         <>
@@ -84,23 +118,28 @@ function AddressForm({checkoutToken}) {
                         </Select>
                     </Grid>
 
-                    {/*<Grid item xs={12} sm={6}>
+                    <Grid item xs={12} sm={6}>
                         <InputLabel>Shipping Subdivision </InputLabel>
-                        <Select value={} fullWidth onChange={}>
-                            <MenuItem key={} value={}>
-                                Select Subdivision
-                            </MenuItem>
+                        <Select value={shippingSubdivision} fullWidth onChange={(event) => setShippingSubdivision(event.target.value)}>
+                            {subdivisions.map((division) => (
+                                <MenuItem key={division.id} value={division.id}>
+                                    {division.label}
+                                </MenuItem>
+                            ))}
+                            
                         </Select>
                     </Grid>
                     
                     <Grid item xs={12} sm={6}>
                         <InputLabel>Shipping Options </InputLabel>
-                        <Select value={} fullWidth onChange={}>
-                            <MenuItem key={} value={}>
-                                Select Option
-                            </MenuItem>
+                        <Select value={shippingOption} fullWidth onChange={(event) => setShippingOption(event.target.value)}>
+                            {options.map((option) => (
+                                <MenuItem key={option.id} value={option.id}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
                         </Select>
-                    </Grid> */}
+                    </Grid> 
 
                 </form>
 
